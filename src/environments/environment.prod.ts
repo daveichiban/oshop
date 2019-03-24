c9118c8d56f30ffdcb5c9e4ea7e8224243ecd3f3
Dave Shield <david.shield@gmail.com>
2019-03-24 18:53:06 +0000

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAp-7a4r1C49lHjB-ezyX6Dg1bGCJobkqM",
+    authDomain: "oshop-1a16b.firebaseapp.com",
+    databaseURL: "https://oshop-1a16b.firebaseio.com",
+    projectId: "oshop-1a16b",
+    storageBucket: "oshop-1a16b.appspot.com",
+    messagingSenderId: "192753699046"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAp-7a4r1C49lHjB-ezyX6Dg1bGCJobkqM",
+    authDomain: "oshop-1a16b.firebaseapp.com",
+    databaseURL: "https://oshop-1a16b.firebaseio.com",
+    projectId: "oshop-1a16b",
+    storageBucket: "oshop-1a16b.appspot.com",
+    messagingSenderId: "192753699046"
+  }
 };
 
 /*
